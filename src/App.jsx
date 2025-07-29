c081c5d6eccd7772603682c3f24bf079f22bf24d
Archibaldo <archibaldo2001@gmail.com>
2025-07-28 19:20:51 -0600

    Weather App with React

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import MainView from './components/MainView'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <MainView />
+    </>
+
+
+
+  )
+
+}
+
+export default App
---
+++ src/components/MainView.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react"
+
+function  MainView () {
+   const [city, setCity] = useState("");
+   const [weather, setWeather] = useState(null);
+   const [error, setError] =  useState(null);
+   const [isLoading, setIsLoading] = useState(false);
+
+   const fetchWeather = async () => {
+      setIsLoading(true);
+      try {
+         const API_KEY = "379a02cbe90444e4abf15914252607";
+         const response = await fetch (
+            `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=es`
+         );
+
+         if (!response.ok) throw new Error ("Ciudad no encontrada");
+         const data = await response.json();
+         setWeather({
+            city: data.location.name,
+            temp: data.current.temp_c,
+            description: data.current.condition.text,
+            humidity: data.current.humidity,
+            icon: data.current.condition.icon
+         });
+         setError(null);
+      } catch(err) {
+         setError(err.message);
+         setWeather(null);
+      } finally {
+         setIsLoading(false);
+      }
+   };
+
+   return (
+
+      <div className="weather-app">
+
+         <div className="card-container">
+            <header className="header">
+               <h1 className="title">Zamna Weather</h1>
+            </header>
+
+            <div className="search-wrapper">
+               
+               <input
+                  type="text"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  onKeyDown={(e)=> {
+                     if (e.key === "Enter") {
+                        fetchWeather();
+                     }
+                  }}
+                  placeholder="Buscar ciudad"
+                  className="search-input"
+               />
+
+               <span className="search-icon" onClick={fetchWeather}>
+                  {isLoading ? "⏳": "🔍"}
+               </span> 
+
+            </div>
+
+
+            {weather && (
+               <div className="weather-card">
+
+                  <div className="location">
+                     <h2 className="city">{weather.city}</h2>
+                  </div>
+
+                  <div className="weather-data">
+
+                     <img src={weather.icon} alt={weather.description} className="weather-icon" />
+                     <p className="temperature">{weather.temp}°C</p>
+                     <p className="description">{weather.description}</p>
+                     <p className="humidity">Humedad: {weather.humidity}%</p>
+                  </div>
+               </div>
+            )}
+
+         </div>
+
+   
+      
+      </div>
+  );
+
+}
+
+export default MainView
